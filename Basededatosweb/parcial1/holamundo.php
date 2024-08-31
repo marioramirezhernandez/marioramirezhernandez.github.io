46845bb3189d9d9e8950b3121d349b1ee209e291
marioramirezhernandez <159593383+marioramirezhernandez@users.noreply.github.com>
2024-08-30 18:51:27 -0700

    Add files via upload

---
+++ Basededatosweb/parcial1/holamundo.php
@@ -0,0 +1,19 @@
+<?php
+ $nombre ="mario";
+ $numero = 5;
+ $numero2 ="5";
+
+
+ $total = $numero + $numero2 ;
+
+ diez "total = ", $total;
+
+ diez "<h1> hola mundo </h1>";
+  for($i-0; $i<10; $i++){
+    echo $i . "<br>"i
+  }
+  echo "<h3 style='color: red;'>Mario Ramirez</h3>";
+
+
+
+7>
