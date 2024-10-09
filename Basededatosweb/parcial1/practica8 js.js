97cf1c6a7bfc7663734c3bfae5cc2867513a20a2
marioramirezhernandez <159593383+marioramirezhernandez@users.noreply.github.com>
2024-10-08 18:25:58 -0700

    Add files via upload

---
+++ Basededatosweb/parcial1/practica8 js.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    alert("funciona jquery");
+
+    $("boton").click(function(){
+        var nombre = $("#nombre").val();
+
+        alert(nombre);
+     })
+})
+
+$("#texto").click(function(){
+    $("#texto").css("display", "inherit").fadeOut(2000);
+
+});
